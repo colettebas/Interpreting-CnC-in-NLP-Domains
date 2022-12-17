db741fd8788223a28d4348bfaaf6eb0e5ae4e3fb
colettebas <colettebas@aol.com>
2022-12-17 12:33:31 -0500

    add code to make plots with normalized SHAP values

--- plot_SHAP_comparison_by_sample.py
+++ plot_SHAP_comparison_by_sample.py
@@ -21,9 +21,23 @@ if __name__ == '__main__':
     erm_SHAP_values = SHAPLoader(erm_SHAP_input_data_filename, erm_SHAP_input_values_filename).SHAP_values
     cnc_SHAP_values = SHAPLoader(cnc_SHAP_input_data_filename, cnc_SHAP_input_values_filename).SHAP_values
 
+    # filter for SHAP values for only this sample
     erm_SHAP_values = erm_SHAP_values.loc[erm_SHAP_values['id'] == sample_id]
     cnc_SHAP_values = cnc_SHAP_values.loc[cnc_SHAP_values['id'] == sample_id]
 
+    # Calculate the normalized SHAP values by dividing them by the sum of the
+    # absolute value of the SHAP values.  We need to take the absolute value because
+    # if we do not then the SHAP values could be larger than the sum.
+    erm_SHAP_values['abs_SHAP_toxic'] = erm_SHAP_values['SHAP_toxic'].abs()
+    cnc_SHAP_values['abs_SHAP_toxic'] = cnc_SHAP_values['SHAP_toxic'].abs()
+
+    erm_sum_SHAP = erm_SHAP_values['abs_SHAP_toxic'].sum()
+    cnc_sum_SHAP = cnc_SHAP_values['abs_SHAP_toxic'].sum()
+
+    erm_SHAP_values['SHAP_toxic_normalized'] = erm_SHAP_values['SHAP_toxic']/erm_sum_SHAP
+    cnc_SHAP_values['SHAP_toxic_normalized'] = cnc_SHAP_values['SHAP_toxic']/cnc_sum_SHAP
+
+    # Plot raw SHAP values
     y_pos = np.arange(len(cnc_SHAP_values['token']))
     width = 0.35  # the width of the bars
 
@@ -38,6 +52,25 @@ if __name__ == '__main__':
     ax.invert_yaxis()  # labels read top-to-bottom
 
     fig.tight_layout()
+    plt.grid(axis = 'y')
 
-    plt.show()
     plt.savefig(str(sample_id) + '_SHAP_comparison.png')
+    plt.show()
+
+    # Plot Normalized SHAP values
+
+    fig, ax = plt.subplots()
+    rects1 = ax.barh(y_pos - width/2, erm_SHAP_values['SHAP_toxic_normalized'], width, label='ERM')
+    rects2 = ax.barh(y_pos + width/2, cnc_SHAP_values['SHAP_toxic_normalized'], width, label='CnC')
+
+    ax.set_xlabel('Normalized SHAP Value')
+    ax.set_title('Normalized SHAP Value by Model for Sample ' + str(sample_id))
+    ax.legend()
+    ax.set_yticks(y_pos, labels=cnc_SHAP_values['token'])
+    ax.invert_yaxis()  # labels read top-to-bottom
+
+    fig.tight_layout()
+    plt.grid(axis = 'y')
+
+    plt.savefig(str(sample_id) + '_SHAP_normalized_comparison.png')
+    plt.show()
--- plot_demographic_violin.py
+++ plot_demographic_violin.py
@@ -13,11 +13,23 @@ if __name__ == '__main__':
     erm_SHAP_values = SHAPLoader(erm_SHAP_input_data_filename, erm_SHAP_input_values_filename).SHAP_values
     cnc_SHAP_values = SHAPLoader(cnc_SHAP_input_data_filename, cnc_SHAP_input_values_filename).SHAP_values
 
+    # Calculate the normalized SHAP values by dividing them by the sum of the
+    # absolute value of the SHAP values.  We need to take the absolute value because
+    # if we do not then the SHAP values could be larger than the sum.
+    erm_SHAP_values['abs_SHAP_toxic'] = erm_SHAP_values['SHAP_toxic'].abs()
+    cnc_SHAP_values['abs_SHAP_toxic'] = cnc_SHAP_values['SHAP_toxic'].abs()
+
+    erm_sum_SHAP = erm_SHAP_values['abs_SHAP_toxic'].sum()
+    cnc_sum_SHAP = cnc_SHAP_values['abs_SHAP_toxic'].sum()
+
+    erm_SHAP_values['Normalized SHAP Value'] = erm_SHAP_values['SHAP_toxic']/erm_sum_SHAP
+    cnc_SHAP_values['Normalized SHAP Value'] = cnc_SHAP_values['SHAP_toxic']/cnc_sum_SHAP
+
     erm_SHAP_values = erm_SHAP_values.rename(columns={"SHAP_toxic": "SHAP Value"})
     erm_SHAP_values['Model'] = 'ERM'
     erm_SHAP_values = erm_SHAP_values.loc[erm_SHAP_values['token'].isin(tokens)]
     erm_SHAP_values['Demographic'] = 'Male'
-    erm_SHAP_values = erm_SHAP_values.drop(['SHAP_not_toxic', 'id', 'token'], axis=1)
+    erm_SHAP_values = erm_SHAP_values.drop(['SHAP_not_toxic', 'id', 'token', 'abs_SHAP_toxic'], axis=1)
 
     cnc_SHAP_values = cnc_SHAP_values.rename(columns={"SHAP_toxic": "SHAP Value"})
     cnc_SHAP_values['Model'] = 'CnC'
@@ -30,9 +42,17 @@ if __name__ == '__main__':
 
     sns.set_theme(style="whitegrid")
 
-    # Draw a nested violinplot and split the violins for easier comparison
+    # Draw violin plot with raw values
     sns.violinplot(data=combined_SHAP_values, x="Demographic", y="SHAP Value", hue="Model",
                    split=True, inner="quart", linewidth=1,
                    palette={"ERM": "b", "CnC": ".85"})
     sns.despine(left=True)
     plt.savefig('sample_violin.png')
+
+    # Draw violin plot with normalized values
+    plt.clf()
+    sns.violinplot(data=combined_SHAP_values, x="Demographic", y="Normalized SHAP Value", hue="Model",
+                   split=True, inner="quart", linewidth=1,
+                   palette={"ERM": "b", "CnC": ".85"})
+    sns.despine(left=True)
+    plt.savefig('normalized_sample_violin.png')
